8d62fbccb079bffadf91907584ad303b9d896758
Danish Naseem <danishnaseem05@gmail.com>
2019-08-09 16:37:21 -0500

    Update main.js

--- site_pages/main.js
+++ site_pages/main.js
@@ -1,9 +1,13 @@
+var navbar = document.querySelector('nav');
+
 function on(n) {
   document.getElementById(`project${n}-modal`).style.display = "block";
+  navbar.style.display = "none";
 }
 
 // Add New Project Cards in the funtion off() below
 function off() {
+  navbar.style.display = "block";
   // Project card 1
   document.getElementById(`project1-modal`).style.display = "none";
   // Project card 2
@@ -12,4 +16,5 @@ function off() {
   document.getElementById(`project3-modal`).style.display = "none";
   // Project card 4
   document.getElementById(`project4-modal`).style.display = "none";
+
 }
