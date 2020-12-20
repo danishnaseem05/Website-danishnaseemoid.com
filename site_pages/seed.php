68b6ecaaba0bc478860ec53fa23fd58e47ae2d21
Danish Naseem <danishnaseem05@users.noreply.github.com>
2020-12-20 17:16:43 -0600

    removed the comment line stating github_url should be part of the array, since github_url column has been moved to a different database table

--- site_pages/seed.php
+++ site_pages/seed.php
@@ -36,8 +36,7 @@
     // project_cards[0..n][1] = language_class
     // project_cards[0..n][2] = card_title
     // project_cards[0..n][3] = card_description
-    // project_cards[0..n][4] = cover
-    // project_cards[0..n][5] = github_url 
+    // project_cards[0..n][4] = cover 
     $project_cards = array(array('Python', 'fab fa-python', 'Web Server', 'Web Server created using sockets in python, runs on localhost:8000 by default, and accepts multiple clients. Saves the html file by keeping the directory structure inside a custom created folder called \'static\', only if the response was a HTTP 200 OK response; otherwise checks for 404 Not Found, and 400 Bad Request errors.', '../pics/project_cards/Web_Server/Capture5.PNG'),
                         array('Java', 'fab fa-java', 'Sorted Doubly Linked List', 'This is the GUI representation of my customly implemented Doubly Linked List. This List is Sorted and only allows unique integer values. Furthermore, the GUI is easily navigable and its purpose is to Add an unique integer while keeping the list sorted, Delete an integer if it exists in the list, and Display All integers currently stored within the list. Adding to and displaying an empty list, deleting an unstored integer, or entering a non-integer value; all would display errors.', '../pics/project_cards/Doubly_Linked_Sorted_List/Capture5.PNG'),
                         array('Ruby', 'fas fa-gem', 'Today\'s Songs', 'This CLI executable program provides the user with top songs of the day. It collects the song data from two different sites, lists them to the user, and enables them to listen to these songs by opening the user\'s preferred song in the chrome browser.', '../pics/project_cards/Todays_songs/Capture2.PNG'),
