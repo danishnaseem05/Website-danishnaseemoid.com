f505936a119dd61ceccf5f803b901d36d9c9ca9f
Danish Naseem <danishnaseem05@users.noreply.github.com>
2020-12-21 01:23:16 -0600

    removed font-awesome import from the web, as it was giving a 403 error response

--- index.php
+++ index.php
@@ -30,7 +30,6 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.1/css/font-awesome.min.css' rel='stylesheet'/>
 
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="fontawesome-free-5.9.0-web/fontawesome-free-5.9.0-web/css/all.min.css">
